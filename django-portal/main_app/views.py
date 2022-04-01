399fc3076c816f603a9b4b1de68a58ead995f139
X <xac923@outlook.com>
2022-03-31 23:28:12 -0700

    app set up complete

---
+++ django-portal/main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
